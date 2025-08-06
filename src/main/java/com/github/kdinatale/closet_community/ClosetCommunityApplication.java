b97d15e21869a33c378ba5e0f6522c5899675243
Kdinatale <90799768+Kdinatale@users.noreply.github.com>
2025-08-05 22:05:55 -0400

    Set up Spring Boot application with MongoDB

---
+++ src/main/java/com/github/kdinatale/closet_community/ClosetCommunityApplication.java
@@ -0,0 +1,13 @@
+package com.github.kdinatale.closet_community;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClosetCommunityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClosetCommunityApplication.class, args);
+	}
+
+}
