8add09c339f4ff548f3732501c7fd44f1615840c
Kdinatale <90799768+Kdinatale@users.noreply.github.com>
2025-08-16 14:26:01 -0400

    Remove auth0 to prevent login issues and begin upper browser styling

--- src/main/java/com/github/kdinatale/closet_community/ClosetCommunityApplication.java
+++ src/main/java/com/github/kdinatale/closet_community/ClosetCommunityApplication.java
@@ -2,25 +2,11 @@ package com.github.kdinatale.closet_community;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.oauth2.core.oidc.user.OidcUser;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 
 @SpringBootApplication
-@RestController
 public class ClosetCommunityApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ClosetCommunityApplication.class, args);
 	}
-//	@GetMapping("/hello")
-//	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-//	    return String.format("Hello %s!", name);
-//	}
-
 
 }
-
--- src/main/java/com/github/kdinatale/closet_community/HomeController.java
+++ src/main/java/com/github/kdinatale/closet_community/HomeController.java
@@ -1,22 +1,17 @@
 package com.github.kdinatale.closet_community;
 
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+//import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Controller for the home page.
  */
 @Controller
 public class HomeController {
-
     @GetMapping("/")
-    public String home(Model model, @AuthenticationPrincipal OidcUser principal) {
-        if (principal != null) {
-            model.addAttribute("profile", principal.getClaims());
-        }
+    public String Index() {
         return "index";
     }
 }
