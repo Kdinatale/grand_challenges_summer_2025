a1c4c361edd06905518fe0af236355c72eeea244
Kdinatale <90799768+Kdinatale@users.noreply.github.com>
2025-08-18 17:34:04 -0400

    Make profile container flex box

--- frontend/closet-community/src/ProfilePage.jsx
+++ frontend/closet-community/src/ProfilePage.jsx
@@ -21,27 +21,46 @@ function ProfilePage() {
                   </div>
                 </div>
               </div>
+              <div className="favorite-clothing-container">
+                <div className="clothing-image-1-container">
+                  <div className="clothing-image-1"></div>
+                </div>
+                <div className="clothing-image-2-container">
+                  <div className="clothing-image-2"></div>
+                </div>
+                <div className="clothing-image-3-container">
+                  <div className="clothing-image-3"></div>
+                </div>
+                <div className="clothing-image-4-container">
+                  <div className="clothing-image-4"></div>
+                </div>
+              </div>
             </div>
-            <div class="favorite-brand-container">
-              <div class="post-it-container">
-                <div class="post-it-1">
-                  <div className="pin-container-1">
-                    <img className="pin-1" src={pinkPin} alt="Pink pin"></img>
+            <div className="favorite-brand-container">
+              <div className="post-it-container">
+                <div className="post-it-1-border">
+                  <div className="post-it-1">
+                    <div className="pin-container-1">
+                      <img className="pin-1" src={pinkPin} alt="Pink pin"></img>
+                    </div>
                   </div>
                 </div>
-                <div class="post-it-2">
-                  {/* <div className="pin-container-2">
-                    <img className="pin-2" src={pinkPin} alt="Pink pin"></img>
-                  </div> */}
+                <div className="post-it-2-border">
+                  <div className="post-it-2"></div>
                 </div>
-                <div class="post-it-3">
-                  <div className="pin-container-3">
-                    <img className="pin-3" src={pinkPin} alt="Pink pin"></img>
+
+                <div className="post-it-3-border">
+                  <div className="post-it-3">
+                    <div className="pin-container-3">
+                      <img className="pin-3" src={pinkPin} alt="Pink pin"></img>
+                    </div>
                   </div>
                 </div>
-                <div class="post-it-4">
-                  <div className="pin-container-4">
-                    <img className="pin-4" src={pinkPin} alt="Pink pin"></img>
+                <div class="post-it-4-border">
+                  <div className="post-it-4">
+                    <div className="pin-container-4">
+                      <img className="pin-4" src={pinkPin} alt="Pink pin"></img>
+                    </div>
                   </div>
                 </div>
               </div>
