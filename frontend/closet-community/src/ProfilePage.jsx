22950621a468b7ea400cf366de92d2438283cb70
Kdinatale <90799768+Kdinatale@users.noreply.github.com>
2025-08-18 16:43:29 -0400

    Create pin image and add base post-it and pin brand section

--- frontend/closet-community/src/ProfilePage.jsx
+++ frontend/closet-community/src/ProfilePage.jsx
@@ -1,7 +1,7 @@
 import Browser from "./Browser.jsx";
 import closedCloset from "./assets/images/closed_closet.png";
 import "./styles/ProfilePage.css";
-
+import pinkPin from "./assets/images/pink_pin.png";
 function ProfilePage() {
   return (
     <>
@@ -18,6 +18,30 @@ function ProfilePage() {
                         <div className="profile-photo-holder"></div>
                       </div>
                     </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="favorite-brand-container">
+              <div class="post-it-container">
+                <div class="post-it-1">
+                  <div className="pin-container-1">
+                    <img className="pin-1" src={pinkPin} alt="Pink pin"></img>
+                  </div>
+                </div>
+                <div class="post-it-2">
+                  {/* <div className="pin-container-2">
+                    <img className="pin-2" src={pinkPin} alt="Pink pin"></img>
+                  </div> */}
+                </div>
+                <div class="post-it-3">
+                  <div className="pin-container-3">
+                    <img className="pin-3" src={pinkPin} alt="Pink pin"></img>
+                  </div>
+                </div>
+                <div class="post-it-4">
+                  <div className="pin-container-4">
+                    <img className="pin-4" src={pinkPin} alt="Pink pin"></img>
                   </div>
                 </div>
               </div>
