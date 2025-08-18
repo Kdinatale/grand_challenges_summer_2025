c5235ee9af081a712e0639c562b5ce179ae10ff0
Kdinatale <90799768+Kdinatale@users.noreply.github.com>
2025-08-18 13:24:57 -0400

    Create basic about me structure and set up closet image

--- frontend/closet-community/src/ProfilePage.jsx
+++ frontend/closet-community/src/ProfilePage.jsx
@@ -1,10 +1,36 @@
 import Browser from "./Browser.jsx";
+import closedCloset from "./assets/images/closed_closet.png";
+import "./styles/ProfilePage.css";
 
 function ProfilePage() {
   return (
     <>
       <Browser>
-        <h1>TEST</h1>
+        <div className="profile-closet-container">
+          <div className="profile-content-container">
+            <div className="about-me-container">
+              <div className="about-me-border">
+                <div className="about-me">
+                  <div className="profile-photo-template">
+                    <div className="about-me-text-container"></div>
+                    <div className="profile-photo-container">
+                      <div className="profile-photo-border">
+                        <div className="profile-photo-holder"></div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="closed-closet-container">
+            <img
+              className="closed-closet"
+              src={closedCloset}
+              alt="Closet with closed door"
+            />
+          </div>
+        </div>
       </Browser>
     </>
   );
