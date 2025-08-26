932bd4566a6e32196ca2fdb3966159a8a0dd72c7
Kdinatale <90799768+Kdinatale@users.noreply.github.com>
2025-08-26 16:53:08 -0400

    Store URL and expiration time to reduce frequency of URL generation

--- backend/src/main/java/com/github/kdinatale/closet_community/Photo.java
+++ backend/src/main/java/com/github/kdinatale/closet_community/Photo.java
@@ -4,6 +4,8 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.URL;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
 import java.util.concurrent.TimeUnit;
 
 import com.google.auth.oauth2.ServiceAccountCredentials;
@@ -14,10 +16,15 @@ import com.google.cloud.storage.StorageOptions;
 class Photo{
     private String bucketName;
     private String objectName;
+    private URL photoUrl;
+    private Instant expirationTime; 
     
     public Photo(String bucketName, String objectName) {
         this.bucketName = bucketName;
         this.objectName = objectName;
+        this.photoUrl = null;
+        this.expirationTime = null;
+        
     }
     
     public void setBucketName(String bucketName) {
@@ -34,21 +41,54 @@ class Photo{
         return this.objectName;
     }
     
+    public void setPhotoUrl(URL photoUrl) {
+        this.photoUrl = photoUrl;
+    }
+    
+    public void setExpirationTime(Instant expirationTime) {
+        this.expirationTime = expirationTime;
+    }
+    
+    public void setExpirationTimeViaTimeGenerated(Instant timeGenerated) {
+        this.expirationTime = timeGenerated.plus(90, ChronoUnit.MINUTES);
+    }
+    
+    public URL getPhotoUrl() {
+        return this.photoUrl;
+    }
+    
+    public Instant getExpirationTime() {
+        return this.expirationTime;
+    }
+    
+    
     public URL getSignedUrl() throws FileNotFoundException, IOException {
-        Storage storage = StorageOptions.newBuilder()
-                .setCredentials(ServiceAccountCredentials.fromStream(new FileInputStream("src/main/resources/closet-community-key.json")))
-                .build()
-                .getService();
+        Instant now = Instant.now();
         
-        BlobInfo blobInfo = BlobInfo.newBuilder(bucketName, objectName).build();
-        
-        URL signedUrl = storage.signUrl(
-                blobInfo,
-                90,
-                TimeUnit.MINUTES,
-                Storage.SignUrlOption.withV4Signature()
-        );
 
-        return signedUrl;
+        if(getPhotoUrl() == null || getExpirationTime() == null ||  now.isAfter(expirationTime)) {
+            
+            Storage storage = StorageOptions.newBuilder()
+                    .setCredentials(ServiceAccountCredentials.fromStream(new FileInputStream("src/main/resources/closet-community-key.json")))
+                    .build()
+                    .getService();
+            
+            BlobInfo blobInfo = BlobInfo.newBuilder(bucketName, objectName).build();
+            
+            URL signedUrl = storage.signUrl(
+                    blobInfo,
+                    90,
+                    TimeUnit.MINUTES,
+                    Storage.SignUrlOption.withV4Signature()
+            );
+            
+            setPhotoUrl(signedUrl);
+            setExpirationTimeViaTimeGenerated(Instant.now());
+    
+            return signedUrl;
+        }
+        else {
+            return this.photoUrl;
+        }
     }
 }
