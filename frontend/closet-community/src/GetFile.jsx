0a114f240102f9726a31074a04ee39aba7477393
Kdinatale <90799768+Kdinatale@users.noreply.github.com>
2025-08-26 17:04:46 -0400

    Pass url so GetFile can be reused

--- frontend/closet-community/src/GetFile.jsx
+++ frontend/closet-community/src/GetFile.jsx
@@ -1,10 +1,7 @@
 import axios from "axios";
 
-async function GetFile() {
-  const userId = "689151d2f2aaa40b4e1b2b2e";
-  const response = await axios.get(
-    `http://localhost:8080/getProfilePhoto/${userId}`
-  );
+async function GetFile(url) {
+  const response = await axios.get(url);
   return response;
 }
 
