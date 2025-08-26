3fd55a1f92969828c3470d16df20182b45110edd
Kdinatale <90799768+Kdinatale@users.noreply.github.com>
2025-08-26 17:04:16 -0400

    Pass userid to ClosetFeedView component

--- frontend/closet-community/src/App.jsx
+++ frontend/closet-community/src/App.jsx
@@ -6,7 +6,10 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path="/closet" element={<ClosetFeedView />}></Route>
+        <Route
+          path="/closet"
+          element={<ClosetFeedView userId="689151d2f2aaa40b4e1b2b2e" />}
+        ></Route>
         <Route
           path="/profile"
           element={<ProfileViewManager userId="689151d2f2aaa40b4e1b2b2e" />}
