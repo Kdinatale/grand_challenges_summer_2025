d9addbc1dcac8b70ffd63c9bd4e879d857c19fab
Kdinatale <90799768+Kdinatale@users.noreply.github.com>
2025-08-26 23:48:43 -0400

    Display clothing in closet feed

--- frontend/closet-community/src/ClosetFeedView.jsx
+++ frontend/closet-community/src/ClosetFeedView.jsx
@@ -3,18 +3,29 @@ import FeedHeader from "./FeedHeader";
 import GetFile from "./GetFile";
 import { useEffect } from "react";
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import "./styles/ClosetFeed.css";
+import SaveIcon from "./assets/images/add_clothing_icon.png";
 
 function ClosetFeedView({ userId }) {
   const [isProfileImageUrl, setIsProfileImageUrl] = useState("");
+  const [isClothingItems, setIsClothingItems] = useState([]);
+  const navigate = useNavigate();
+
+  const handleClickAddClothingItem = () => {
+    console.log("CLICK");
+    navigate("/addClothingItem", { viewTransition: true });
+  };
 
   useEffect(() => {
-    // async function getClothingItems() {
-    //   const response = await GetFile(
-    //     `http://localhost:8080/getClothingItemsOrderedByTime/${userId}`
-    //   );
-    //   console.log(response.data);
-    // }
-    // getClothingItems();
+    async function getClothingItems() {
+      const response = await GetFile(
+        `http://localhost:8080/getClothingItemsOrderedByTime/${userId}`
+      );
+      setIsClothingItems([SaveIcon].concat(response.data));
+      console.log(response.data);
+    }
+
     async function getProfilePhoto() {
       const response = await GetFile(
         `http://localhost:8080/getProfilePhoto/${userId}`
@@ -23,23 +34,43 @@ function ClosetFeedView({ userId }) {
       console.log(response.data);
     }
     getProfilePhoto();
+    getClothingItems();
   }, []);
 
   return (
     <>
-      <div className="closet-feed-view-container">
-        <Browser>
+      <Browser>
+        <div className="closet-feed-container">
           <FeedHeader
             profileImage={
               <img className="profile-photo-img" src={isProfileImageUrl}></img>
             }
             Username={"kwd2002"}
           />
-          <div className="closet-feed-container">
-            <div className="clothing-item-container"></div>
+          <div className="clothing-item-container">
+            {isClothingItems.map((clothingItem, index) => {
+              const isAddIcon = index === 0;
+              const className = isAddIcon
+                ? "closet-feed-item closet-feed-item-add-icon"
+                : "closet-feed-item";
+              return isAddIcon ? (
+                <div
+                  key={index}
+                  id={className}
+                  className={className}
+                  onClick={handleClickAddClothingItem}
+                >
+                  <img src={clothingItem} alt={`Item ${index}`} />
+                </div>
+              ) : (
+                <div key={index} id={className} className={className}>
+                  <img src={clothingItem} alt={`Item ${index}`} />
+                </div>
+              );
+            })}
           </div>
-        </Browser>
-      </div>
+        </div>
+      </Browser>
     </>
   );
 }
