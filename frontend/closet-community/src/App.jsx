074b8226ac622e515d87de6fdf0fbc5ceaab3a3d
Kdinatale <90799768+Kdinatale@users.noreply.github.com>
2025-08-27 21:55:41 -0400

    Remove unnecessary userId specification

--- frontend/closet-community/src/App.jsx
+++ frontend/closet-community/src/App.jsx
@@ -7,10 +7,7 @@ function App() {
   return (
     <>
       <Routes>
-        <Route
-          path="/closet/:userId"
-          element={<ClosetFeedView userId="689151d2f2aaa40b4e1b2b2e" />}
-        ></Route>
+        <Route path="/closet/:userId" element={<ClosetFeedView />}></Route>
         <Route
           path="/profile/"
           element={<ProfileViewManager userId="689151d2f2aaa40b4e1b2b2e" />}
