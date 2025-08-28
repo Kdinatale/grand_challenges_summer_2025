aa89ffb81e9657900187e864b87852bf871c0b08
Kdinatale <90799768+Kdinatale@users.noreply.github.com>
2025-08-27 21:58:48 -0400

    Add method to upload clothing post

--- backend/src/main/java/com/github/kdinatale/closet_community/ClothingController.java
+++ backend/src/main/java/com/github/kdinatale/closet_community/ClothingController.java
@@ -10,15 +10,21 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
 
 @Controller
 public class ClothingController {
     public final ClothingItemService clothingItemService;
-    
-    public ClothingController(ClothingItemService clothingItemService) {
+    final UploadService uploadService;
+
+    public ClothingController(ClothingItemService clothingItemService, UploadService uploadService) {
         this.clothingItemService = clothingItemService;
+        this.uploadService = uploadService;
+
     }
     
     @CrossOrigin(origins = "http://localhost:5173")
@@ -32,6 +38,40 @@ public class ClothingController {
             itemUrls.add(clothingItems.get(i).getClothingItemPhoto().getSignedUrl().toString());
         }
         return itemUrls;
+    }
+    
+    @CrossOrigin(origins = "http://localhost:5173")
+    @PostMapping("/postClothingItem/{userId}")
+    @ResponseBody
+    public void postClothingItem(@PathVariable String userId, @RequestParam("image") MultipartFile image, @RequestParam("caption") String caption) throws IOException {
+        String clothingFolderName = "clothing-images";
+
+        String fileExtension;
+        switch(image.getContentType()) {
+            case "image/jpeg":
+                fileExtension = ".jpg";
+                break;
+            case "image/png":
+                fileExtension = ".png";
+                break;
+            default:
+                fileExtension = "";
+        }
+        
+        ClothingItem item = new ClothingItem(ClothingType.DRESS, "pink", Size.LARGE, userId, caption);
+        
+        clothingItemService.addClothingItem(item);
+
+        String objectName = clothingFolderName + "/" + userId+ "clothing_item" + item.getId() + fileExtension;
+
+        uploadService.uploadImage(image, objectName);
+
+        Photo newPhoto = new Photo("closet-community-bucket", objectName);
+        
+        item.setClothingItemPhoto(newPhoto);
+        clothingItemService.saveClothingItem(item);
+        
+        
     }
 
 }
