515ee89f2eebf4b2a55de801389558354722db60
Kdinatale <90799768+Kdinatale@users.noreply.github.com>
2025-08-28 12:55:25 -0400

    Add route to individual post view

--- frontend/closet-community/src/App.jsx
+++ frontend/closet-community/src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import ClosetFeedView from "./ClosetFeedView";
 import ProfileViewManager from "./ProfileViewManager";
 import CreatePost from "./CreatePost";
+import ViewPost from "./viewPost";
 
 function App() {
   return (
@@ -15,6 +16,10 @@ function App() {
         <Route
           path="/addClothingItem/:userId"
           element={<CreatePost userId="689151d2f2aaa40b4e1b2b2e" />}
+        ></Route>
+        <Route
+          path="/viewClothingItem/"
+          element={<ViewPost userId="689151d2f2aaa40b4e1b2b2e" />}
         ></Route>
       </Routes>
     </>
