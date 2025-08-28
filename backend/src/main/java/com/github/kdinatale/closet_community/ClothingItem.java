f1d04be5d713c30abd874d909ab4938656ff4406
Kdinatale <90799768+Kdinatale@users.noreply.github.com>
2025-08-27 21:57:56 -0400

    Add caption to clothingItem POJO

--- backend/src/main/java/com/github/kdinatale/closet_community/ClothingItem.java
+++ backend/src/main/java/com/github/kdinatale/closet_community/ClothingItem.java
@@ -15,14 +15,16 @@ class ClothingItem{
    private LocalDateTime createdAt;
    private String userId;
    private Photo clothingItemPhoto;
+   private String caption;
 
-   public ClothingItem(ClothingType type, String color, Size size, LocalDateTime createdAt, String userId, Photo clothingItemPhoto) {
+   public ClothingItem(ClothingType type, String color, Size size, String userId, String caption) {
        this.type = type;
        this.color = color;
        this.size = size;
-       this.createdAt = createdAt;
+       this.createdAt = LocalDateTime.now();
        this.userId = userId;
-       this.clothingItemPhoto = clothingItemPhoto;
+       this.clothingItemPhoto = null;
+       this.caption = caption;
    }
    public ClothingType getType() {
        return this.type;
@@ -64,6 +66,16 @@ class ClothingItem{
    
    public Photo getClothingItemPhoto() {
        return this.clothingItemPhoto;
+   }
+   public String getCaption() {
+       return this.caption;
+   }
+   public void setCaption(String caption) {
+       this.caption = caption;
+   }
+   
+   public String getId() {
+       return id;
    }
    
 }
