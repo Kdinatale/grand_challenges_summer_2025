ae24543bc9b35ed74457b6829b958cbaec8a0e86
Kdinatale <90799768+Kdinatale@users.noreply.github.com>
2025-08-27 21:55:58 -0400

    Add image and caption post handling

--- frontend/closet-community/src/PostModal.jsx
+++ frontend/closet-community/src/PostModal.jsx
@@ -3,24 +3,35 @@ import photoIcon from "./assets/images/missing_photo_icon.png";
 // import cancelButton from "./assets/images/cancel_button.png";
 // import postButton from "./assets/images/post_button.png";
 import selectPhoto from "./assets/images/select_photo.png";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { useState } from "react";
 import { useRef } from "react";
+import UploadFile from "./UploadFile";
 
-function PostModal({ userId }) {
+function PostModal({ url }) {
   const [postCaption, setPostCaption] = useState("");
   const [postImageURL, setPostImageURL] = useState(photoIcon);
   const fileInputRef = useRef();
+  const { userId } = useParams();
 
   const navigate = useNavigate();
 
   const handleCancelClick = () => {
     navigate(`/closet/${userId}`);
   };
-  function postClothingItem(e) {
+  const post = async (e) => {
     e.preventDefault();
-    console.log("test");
-  }
+    const formData = new FormData();
+    console.log("FORM DATA: " + formData);
+    const file = fileInputRef.current.files[0];
+    formData.append("image", file);
+    formData.append("caption", postCaption);
+    console.log("CAPTION " + formData.get("caption"));
+    console.log("IMAGE" + formData.get("image"));
+    const response = await UploadFile(formData, url);
+    console.log(response);
+    navigate(`/closet/${userId}`);
+  };
 
   function storePostImage() {
     const file = fileInputRef.current.files[0];
@@ -33,7 +44,7 @@ function PostModal({ userId }) {
       <div className="post-container">
         <div className="modal-container-border">
           <div className="modal-container">
-            <form className="modal-container-form" onSubmit={postClothingItem}>
+            <form className="modal-container-form" onSubmit={post}>
               <div className="upper-modal-container">
                 <div className="image-button-container">
                   <div className="image-section">
