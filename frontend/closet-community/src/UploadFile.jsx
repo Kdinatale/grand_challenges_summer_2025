2fff93926586446a2d07cebe821630bc1f97c7fb
Kdinatale <90799768+Kdinatale@users.noreply.github.com>
2025-08-27 21:56:46 -0400

    Make file upload generalizable

--- frontend/closet-community/src/UploadFile.jsx
+++ frontend/closet-community/src/UploadFile.jsx
@@ -1,18 +1,12 @@
 import axios from "axios";
 
-async function UploadFile(file) {
-  const formData = new FormData();
-  formData.append("file", file);
-  const userId = "689151d2f2aaa40b4e1b2b2e";
-  const response = await axios.post(
-    `http://localhost:8080/uploadProfilePhoto/${userId}`,
-    formData,
-    {
-      headers: {
-        "Content-Type": "multipart/form-data",
-      },
-    }
-  );
+async function UploadFile(formData, url) {
+  console.log("UPLOADFILE URL: " + url);
+  const response = await axios.post(url, formData, {
+    headers: {
+      "Content-Type": "multipart/form-data",
+    },
+  });
   console.log(response);
 }
 export default UploadFile;
