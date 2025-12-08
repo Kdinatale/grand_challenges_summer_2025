37aedddfc0d84a972ad8d20ebcefd365f43f0f3b
Kdinatale <90799768+Kdinatale@users.noreply.github.com>
2025-12-08 00:59:29 -0500

    Adjust path name

--- frontend/closet-community/src/Home.jsx
+++ frontend/closet-community/src/Home.jsx
@@ -7,7 +7,7 @@ function Home() {
   const { loginWithRedirect } = useAuth0();
 
   useEffect(() => {
-    loginWithRedirect({ redirectUri: "http://localhost:5173/test" });
+    loginWithRedirect({ redirectUri: "http://localhost:5173/profile" });
   }, []);
 
   return <></>;
