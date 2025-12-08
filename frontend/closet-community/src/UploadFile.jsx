a52ec2971eb3d85f33545197131a2f0654f4b835
Kdinatale <90799768+Kdinatale@users.noreply.github.com>
2025-12-08 01:00:12 -0500

    Pass token in HTTP post request

--- frontend/closet-community/src/UploadFile.jsx
+++ frontend/closet-community/src/UploadFile.jsx
@@ -2,9 +2,12 @@ import axios from "axios";
 
 async function UploadFile(formData, url) {
   console.log("UPLOADFILE URL: " + url);
+  const token = localStorage.getItem("token");
+  if (!token) return;
   const response = await axios.post(url, formData, {
     headers: {
       "Content-Type": "multipart/form-data",
+      Authorization: `Bearer ${token}`,
     },
   });
   console.log(response);
