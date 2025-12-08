21d05f109e210c18a1fde73a3a17a5ca81abd7e7
Kdinatale <90799768+Kdinatale@users.noreply.github.com>
2025-12-08 01:01:27 -0500

    Remove unnecessary user id from path and as prop

--- frontend/closet-community/src/CreatePost.jsx
+++ frontend/closet-community/src/CreatePost.jsx
@@ -3,14 +3,11 @@ import "./styles/CreatePost.css";
 import Browser from "./Browser";
 import PostModal from "./PostModal";
 
-function CreatePost({ userId }) {
-  const customUrl = `http://localhost:8080/postClothingItem/${userId}`;
+function CreatePost() {
+  const customUrl = `http://localhost:8080/postClothingItem/`;
   return (
     <>
-      <Browser
-        children={<PostModal url={customUrl} />}
-        rightUrl={`/closet/${userId}`}
-      />
+      <Browser children={<PostModal url={customUrl} />} rightUrl={`/closet/`} />
     </>
   );
 }
