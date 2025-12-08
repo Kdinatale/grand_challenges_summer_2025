302e0ca45194343c9f26c21452a48b16f2d7ba9a
Kdinatale <90799768+Kdinatale@users.noreply.github.com>
2025-12-08 00:58:58 -0500

    Clean up comments and adjust path naming conventions

--- frontend/closet-community/src/App.jsx
+++ frontend/closet-community/src/App.jsx
@@ -5,30 +5,31 @@ import CreatePost from "./CreatePost";
 import ViewPost from "./viewPost";
 import Home from "./Home";
 import Test from "./Test";
+import { useAccessToken } from "./useAccessToken";
+import { useEffect } from "react";
+
 function App() {
+  const getToken = useAccessToken();
+
+  useEffect(() => {
+    const getTokenAndStorage = async () => {
+      const token = await getToken();
+      if (!token) return;
+      localStorage.setItem("token", token);
+      console.log("Token: ", localStorage.getItem("token"));
+    };
+
+    getTokenAndStorage();
+  }, [getToken]);
+
   return (
     <>
       <Routes>
-        {/* <Route path="/closet/:userId" element={<ClosetFeedView />}></Route>
-        <Route
-          path="/profile/"
-        <Route path="/closet" element={<ClosetFeedView />}></Route> */}
-        {/* <Route
-          path="/profile"
-          element={<ProfileViewManager userId="689151d2f2aaa40b4e1b2b2e" />}
-        ></Route>
-        <Route
-          path="/addClothingItem/:userId"
-          element={<CreatePost userId="689151d2f2aaa40b4e1b2b2e" />}
-        ></Route>
-        <Route
-          path="/viewClothingItem/"
-          element={<ViewPost userId="689151d2f2aaa40b4e1b2b2e" />}
-        ></Route>
-        ></Route> */}
-        <Route path="/test" element={<ProfileViewManager />}></Route>
+        <Route path="/closet/" element={<ClosetFeedView />}></Route>
+        <Route path="/addClothingItem/" element={<CreatePost />}></Route>
+        <Route path="/viewClothingItem/" element={<ViewPost />}></Route>
+        <Route path="/profile" element={<ProfileViewManager />}></Route>
         <Route path="/" element={<Home></Home>}></Route>
-        {/* <Route path="/test" element={<Test></Test>}></Route> */}
       </Routes>
     </>
   );
