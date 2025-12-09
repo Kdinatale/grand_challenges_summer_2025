a1efa86053e003c0a1eab1b6b7bddc6c2b51b857
Kdinatale <90799768+Kdinatale@users.noreply.github.com>
2025-12-08 21:23:56 -0500

    Clean up comments

--- backend/src/main/java/com/github/kdinatale/closet_community/User.java
+++ backend/src/main/java/com/github/kdinatale/closet_community/User.java
@@ -14,32 +14,9 @@ class User{
     private String authId;
     
     public User(String authId) {
-//        this.lastName = lastName;
-//        this.firstName = firstName;
-//        this.email = email;
 //        this.userName = userName;
         this.authId = authId;
     }
-//    
-//    public String getFirstName() {
-//        return this.firstName;
-//    }
-//    public void setFirstName(String firstName) {
-//        this.firstName = firstName;
-//    }
-//    
-//    public String getLastName() {
-//        return this.lastName;
-//    }
-//    public void setLastName(String lastName) {
-//        this.lastName = lastName;
-//    }
-//    public String getEmail(String email) {
-//        return this.email;
-//    }
-//    public void setEmail(String email) {
-//        this.email = email;
-//    }
 //    public void setUserName(String userName) {
 //        this.userName = userName;
 //    }
